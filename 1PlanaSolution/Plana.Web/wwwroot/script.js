7e3d65e84964d7ad6f123d2d31e1ae66d8796c0b
kristya <shiriagina@yahoo.com>
2020-12-13 23:35:38 +0100

    added status for the tasks of the sprint backlog

--- 1PlanaSolution/Plana.Web/wwwroot/script.js
+++ 1PlanaSolution/Plana.Web/wwwroot/script.js
@@ -11,3 +11,24 @@ function renderjQueryComponents() {
     });
 
 }
+
+
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+function CollapseTableRow() {
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+}
+
+function GetName() {
+    alert("Name");
+}
